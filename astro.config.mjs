bed545068432954518a2d50f4a2c5136494269db
Kurniadi17 <kurniadikurniadi1@gmail.com>
2024-09-04 17:25:51 +0700

    fix: and change the base URL for the vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://astro-ad-engine.vercel.app',
   integrations: [react(), mdx(), sitemap()],
 });
