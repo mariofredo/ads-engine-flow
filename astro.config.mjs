ec79293eef7b56a2ae18e89d6237875c1a0289fe
Kurniadi17 <kurniadikurniadi1@gmail.com>
2024-09-04 17:30:42 +0700

    fix: and change the base URL for the vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import {ViteEnv} from 'vite';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://astro-ad-engine.vercel.app',
   integrations: [react(), mdx(), sitemap()],
+  vite: {
+    define: {
+      __BASE_URL__: JSON.stringify(process.env.VITE_FE_URL),
+    },
+  },
 });
