feb3e7265631649d2be500d3e7265ae8403aea20
mariofredooo <mariotannuwijaya@gmail.com>
2024-10-08 10:16:07 +0700

    modify css import bg

--- public/js/search.js
+++ public/js/search.js
@@ -28,11 +28,6 @@ document.addEventListener('DOMContentLoaded', function () {
   `;
   importCSS(`${baseURL}/css/search.css`);
   document
-    .querySelector('.search_item_input')
-    .addEventListener('input', function (e) {
-      console.log(e.target.value);
-    });
-  document
     .querySelector('.search_box')
     .addEventListener('submit', function (e) {
       e.preventDefault();
