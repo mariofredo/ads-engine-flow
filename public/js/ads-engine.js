061ae6542156b5609a1149c96f082943da647cb1
mariofredooo <mariotannuwijaya@gmail.com>
2024-12-02 14:09:53 +0700

    create: modify script ads-engine

--- public/js/ads-engine.js
+++ public/js/ads-engine.js
@@ -3,14 +3,16 @@ document.addEventListener('DOMContentLoaded', function () {
   async function callHotelData(payload) {
     console.log('call hotel data');
     const response = await fetch(
-      'https://typesense.flowtheroom.com/multi_search?region=hk',
+      'https://api-v6.flowtheroom.com/core/search?region=hk&platform=web&lang=zh-hk',
       {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Accept: 'application/json',
           'x-typesense-api-key': '8juFWVOoEDP5Pm1UlpQEFP42rvBdKu5l',
           'x-typesense-cluster-id': 'm2wlnapq6ue95y3zp',
+          Accept: 'application/json',
+          origin: 'https://app.flowtheroom.com',
+          referer: 'https://app.flowtheroom.com',
         },
         body: JSON.stringify(payload),
       }
@@ -33,6 +35,8 @@ document.addEventListener('DOMContentLoaded', function () {
   }
   const urlParams = new URLSearchParams(window.location.search);
   const search = urlParams.get('search');
+  const id = urlParams.get('id');
+  const type = urlParams.get('type');
   // Define base URL
   const baseURL = `https://ads-engine-flow.vercel.app`;
   // const baseURL = `http://localhost:4321`;
@@ -156,76 +160,90 @@ document.addEventListener('DOMContentLoaded', function () {
          </a>
           `);
         case 'three':
-          const {results} = await callHotelData({
-            searches: [
-              {
-                collection: 'space_HK_1_PROD',
-                page: 1,
-                include_fields:
-                  'slug,_id,name,location.area,location.district,location.country,location.city,location.state,_geoloc,_tags,tags,images,ranking,config,id,top_comments,top_comments_average_rating,status.min_prices,frame_style,status',
-                per_page: 10,
-                query_by:
-                  'name,location.address,location.area.name,location.district.name,location.meta_district.name,location.city.name,location.state.name,amenities.name,tags.content',
-                sort_by:
-                  '_text_match:desc,_eval([(ranking.is_promotion:true):10,(ranking.is_featured:true):9]):desc,ranking.normalized_score:desc',
-                override_tags: 'deals_today-s-deal',
-                filter_by:
-                  '_tags:=live && config.show.is_web_show:=true && (config.show.status:!=UNLISTED) && ((tags.slug:=today-s-deal))',
-                q: search || '*',
-              },
-            ],
-          });
+          // const {results} = await callHotelData({
+          //   searches: [
+          //     {
+          //       collection: 'space_HK_1_PROD',
+          //       page: 1,
+          //       include_fields:
+          //         'slug,_id,name,location.area,location.district,location.country,location.city,location.state,_geoloc,_tags,tags,images,ranking,config,id,top_comments,top_comments_average_rating,status.min_prices,frame_style,status',
+          //       per_page: 10,
+          //       query_by:
+          //         'name,location.address,location.area.name,location.district.name,location.meta_district.name,location.city.name,location.state.name,amenities.name,tags.content',
+          //       sort_by:
+          //         '_text_match:desc,_eval([(ranking.is_promotion:true):10,(ranking.is_featured:true):9]):desc,ranking.normalized_score:desc',
+          //       override_tags: 'deals_today-s-deal',
+          //       filter_by:
+          //         '_tags:=live && config.show.is_web_show:=true && (config.show.status:!=UNLISTED) && ((tags.slug:=today-s-deal))',
+          //       q: search || '*',
+          //     },
+          //   ],
+          // });
 
-          const arr = results[0].hits;
-          const adElementHref = adElement.getAttribute('data-href');
-          const cards = arr.map(
-            (item) => `<a href="${adElementHref}?a_aid=671f70eb411e" target="_blank" class="card-ad">
-           <img class="card-ad-img" src="${
-             item.document.images[0]
-           }" alt="blog-placeholder">
-           <div class="card-ad-content">
-             <h1 class="card-ad-title">${item.document.name.en}</h1>
-             <div class="card-ad-location">
-               <img class="card-ad-location-icon" src="${baseURL}/location-icon.svg" alt="location_icon">
-               <p class="card-ad-location-text">${
-                 item.document.location.district.name.en +
-                 ', ' +
-                 item.document.location.state.name.en
-               }</p>
-             </div>
-                <div class="card-ad-review">
-                    <div class="star-rating">
-                        ${generateStarRating(
-                          item.document.ranking.user_rating ?? 0
-                        )}
-                    </div>
-                    <p class="card-ad-review-text"><span id="rating value">${
-                      item.document.ranking.user_rating ?? 0
-                    }</span> / 5 <span class="card-ad-review-count">(${
-              item.document.ranking.total_comments
-            } Reviews)</span></p>
-                  </div>
-             <div class="card-ad-tag-list">
-              ${item.document.tags
-                .map(
-                  (tag) =>
-                    `<div class="card-ad-tag-bubble">${tag.content.en}</div>`
-                )
-                .join('')}
-             </div>
-             <div class="card-ad-available">
-               <div class="card-ad-available-text">Available date</div>
-               <p class="card-ad-available-date">10-12-2024</p>
-             </div>
-             <div class="card-ad-button"><span class="card-ad-button-text">HK$ 50</span></div>
-           </div>
-         </a>`
-          );
-          return (adElement.innerHTML = `
-              <div class="card-ad-container">
-                  ${cards.join('')}
-              </div>
-              `);
+          const data = await callHotelData({
+            space_type_tab: 'rest',
+            sort_by: 's_default',
+            per_page: 6,
+            page: 1,
+            include_buttons: false,
+            query: {
+              id,
+              type,
+            },
+          });
+          console.log(data, 'data');
+        //   const arr = results[0].hits;
+        //   const adElementHref = adElement.getAttribute('data-href');
+        //   const cards = arr.map(
+        //     (
+        //       item
+        //     ) => `<a href="${adElementHref}?a_aid=671f70eb411e" target="_blank" class="card-ad">
+        //    <img class="card-ad-img" src="${
+        //      item.document.images[0]
+        //    }" alt="blog-placeholder">
+        //    <div class="card-ad-content">
+        //      <h1 class="card-ad-title">${item.document.name.en}</h1>
+        //      <div class="card-ad-location">
+        //        <img class="card-ad-location-icon" src="${baseURL}/location-icon.svg" alt="location_icon">
+        //        <p class="card-ad-location-text">${
+        //          item.document.location.district.name.en +
+        //          ', ' +
+        //          item.document.location.state.name.en
+        //        }</p>
+        //      </div>
+        //         <div class="card-ad-review">
+        //             <div class="star-rating">
+        //                 ${generateStarRating(
+        //                   item.document.ranking.user_rating ?? 0
+        //                 )}
+        //             </div>
+        //             <p class="card-ad-review-text"><span id="rating value">${
+        //               item.document.ranking.user_rating ?? 0
+        //             }</span> / 5 <span class="card-ad-review-count">(${
+        //       item.document.ranking.total_comments
+        //     } Reviews)</span></p>
+        //           </div>
+        //      <div class="card-ad-tag-list">
+        //       ${item.document.tags
+        //         .map(
+        //           (tag) =>
+        //             `<div class="card-ad-tag-bubble">${tag.content.en}</div>`
+        //         )
+        //         .join('')}
+        //      </div>
+        //      <div class="card-ad-available">
+        //        <div class="card-ad-available-text">Available date</div>
+        //        <p class="card-ad-available-date">10-12-2024</p>
+        //      </div>
+        //      <div class="card-ad-button"><span class="card-ad-button-text">HK$ 50</span></div>
+        //    </div>
+        //  </a>`
+        //   );
+        //   return (adElement.innerHTML = `
+        //       <div class="card-ad-container">
+        //           ${cards.join('')}
+        //       </div>
+        //       `);
       }
     }
     handleRenderCard();
--- public/js/search.js
+++ public/js/search.js
@@ -9,8 +9,58 @@ document.addEventListener('DOMContentLoaded', function () {
     // Append the link element to the head of the document
     document.head.appendChild(link);
   }
-  const baseURL = `https://ads-engine-flow.vercel.app`;
-  // const baseURL = `http://localhost:4321`;
+
+  async function handleSearch() {
+    // e.preventDefault();
+    const search = document.querySelector('.search_item_input').value;
+    const region = 'hk';
+    // const baseURL = `https://ads-engine-flow.vercel.app`;
+    const url = `https://typesense.flowtheroom.com/multi_search?region=${region}`;
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-typesense-api-key': '8juFWVOoEDP5Pm1UlpQEFP42rvBdKu5l',
+        'x-typesense-cluster-id': 'm2wlnapq6ue95y3zp',
+        Accept: 'application/json',
+        origin: 'https://app.flowtheroom.com',
+        referer: 'https://app.flowtheroom.com',
+      },
+      body: JSON.stringify({
+        searches: [
+          {
+            collection: 'space_external_query_suggestions_HK_1_PROD',
+            q: search,
+            filter_by: 'location_groups:=[ALL] && category:!=AMENITY',
+            per_page: 5,
+            query_by: 'query',
+            sort_by: '_text_match:desc,count:desc',
+            include_fields:
+              '_id,category,category_text,lang,location_groups,query',
+          },
+        ],
+      }),
+    });
+    const data = await response.json();
+    console.log(data, 'data');
+    const resultList =
+      data.results.map(
+        (item) =>
+          `<a href="${baseURL}/list?id=${item.hits[0].document._id}&type=${item.hits[0].document.category}" class="search_result_item">
+        <img class="search_result_icon" src="${baseURL}/bed_icon.svg" alt="">
+        <p class="search_result_item_title">${item.hits[0].document.query}</p>
+      </a>`
+      ) || `<p>No results found.</p>`;
+    const searchBox = document.querySelector('.search_box');
+    const searchResult = document.createElement('div');
+    searchResult.classList.add('search_result');
+    searchResult.innerHTML = resultList;
+
+    searchBox.appendChild(searchResult); // Appending the new results
+  }
+
+  // const baseURL = `https://ads-engine-flow.vercel.app`;
+  const baseURL = `http://localhost:4321`;
   document.getElementById('flow-search').innerHTML = `<form class="search_box">
       <img class="search_logo" src="${baseURL}/flow_logo.svg" alt="">
       <div class="search_input">
@@ -34,22 +84,17 @@ document.addEventListener('DOMContentLoaded', function () {
       </div>
     </form>
   `;
-  importCSS(`${baseURL}/css/search.css`);
+  importCSS(`${baseURL}/css/search.scss`);
   document
     .querySelector('.search_box')
-    .addEventListener('submit', function (e) {
+    .addEventListener('submit', async function (e) {
       e.preventDefault();
-      window.open(
-        `${baseURL}/list?a_aid=671f70eb411e&search=${
-          document.querySelector('.search_item_input').value
-        }`
-      );
+      await handleSearch();
     });
-  document.querySelector('.search_btn').addEventListener('click', function () {
-    window.open(
-      `${baseURL}/list?a_aid=671f70eb411e&search=${
-        document.querySelector('.search_item_input').value
-      }`
-    );
-  });
+  document
+    .querySelector('.search_btn')
+    .addEventListener('click', async function (e) {
+      e.preventDefault();
+      await handleSearch(e);
+    });
 });
