b9df5a2741ba61f1ea939a2ff8b6a5ba7602e9f4
mariofredooo <mariotannuwijaya@gmail.com>
2024-12-02 16:53:33 +0700

    modify ads-engine.js script

--- public/js/ads-engine.js
+++ public/js/ads-engine.js
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   adElements.forEach((adElement) => {
     const adElementType = adElement.getAttribute('data-type');
-    const adElementHref = adElement.getAttribute('data-href');
+    // const adElementHref = adElement.getAttribute('data-href');
     console.log(`Processing ad element of type: ${adElementType}`); // Log the type of each ad element
 
     // Helper function to generate star rating HTML
@@ -82,173 +82,63 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Loop melalui setiap elemen dan isi dengan konten iklan
     async function handleRenderCard() {
-      switch (adElementType) {
-        case 'one':
-          const ratingOne = 3.4;
-          // callHotelData({
-          //   searches: [
-          //     {
-          //       collection: 'space_HK_1_PROD',
-          //       page: 1,
-          //       include_fields:
-          //         'slug,_id,name,location.area,location.district,location.country,location.city,location.state,_geoloc,_tags,tags,images,ranking,config,id,top_comments,top_comments_average_rating,status.min_prices,frame_style,status',
-          //       per_page: 10,
-          //       query_by:
-          //         'name,location.address,location.area.name,location.district.name,location.meta_district.name,location.city.name,location.state.name,amenities.name,tags.content',
-          //       sort_by:
-          //         '_text_match:desc,_eval([(ranking.is_promotion:true):10,(ranking.is_featured:true):9]):desc,ranking.normalized_score:desc',
-          //       override_tags: 'deals_today-s-deal',
-          //       filter_by:
-          //         '_tags:=live && config.show.is_web_show:=true && (config.show.status:!=UNLISTED) && ((tags.slug:=today-s-deal))',
-          //       q: adElement.getAttribute('data-search') || '*',
-          //     },
-          //   ],
-          // });
-
-          return (adElement.innerHTML = `
-              <a href="${adElementHref}?a_aid=671f70eb411e" target="_blank" class="card-ad">
-                <img class="card-ad-img" src="${baseURL}/blog-placeholder-1.jpg" alt="blog-placeholder">
-                <div class="card-ad-content">
-                  <h1 class="card-ad-title">Hotel Purple</h1>
-                  <div class="card-ad-location">
-                    <img class="card-ad-location-icon" src="${baseURL}/location-icon.svg" alt="location_icon">
-                    <p class="card-ad-location-text">Grand Slipi Tower</p>
-                  </div>
-                  <div class="card-ad-review">
-                    <div class="star-rating">
-                        ${generateStarRating(ratingOne)}
-                    </div>
-                    <p class="card-ad-review-text"><span id="rating value">${ratingOne}</span> / 5 <span class="card-ad-review-count">(243 Reviews)</span></p>
-                  </div>
-                  <div class="card-ad-tag-list">
-                    <div class="card-ad-tag-bubble">Bathrooms</div>
-                    <div class="card-ad-tag-bubble">Bedrooms</div>
-                    <div class="card-ad-tag-bubble">Parking</div>
-                  </div>
-                  <div class="card-ad-available">
-                    <div class="card-ad-available-text">Available date</div>
-                    <p class="card-ad-available-date">10-12-2024</p>
-                  </div>
-                  <div class="card-ad-button"><span class="card-ad-button-text">HK$ 50</span></div>
-                </div>
-              </a>
-        `);
-        case 'two':
-          const ratingSecond = 4;
-          return (adElement.innerHTML = `
-          <a href="${adElementHref}?a_aid=671f70eb411e" target="_blank" class="card-ad card-ad-row">
-           <img class="card-ad-img" src="${baseURL}/blog-placeholder-1.jpg" alt="blog-placeholder">
-           <div class="card-ad-content">
-             <h1 class="card-ad-title">Hotel Purple</h1>
-             <div class="card-ad-location">
-               <img class="card-ad-location-icon" src="${baseURL}/location-icon.svg" alt="location_icon">
-               <p class="card-ad-location-text">Grand Slipi Tower</p>
-             </div>
-             <div class="card-ad-review">
+      const data = await callHotelData({
+        space_type_tab: 'rest',
+        sort_by: 's_default',
+        per_page: 6,
+        page: 1,
+        include_buttons: false,
+        query: {
+          id,
+          type: type === 'NAME' ? 'SPACE' : type,
+        },
+      });
+      const {spaces} = data;
+      const cards = spaces.map(
+        (item) => `<a href="https://app.flowtheroom.com/hk/en/${
+          item.slug
+        }/hotel" target="_blank" class="card-ad">
+       <img class="card-ad-img" src="${item.images[0]}" alt="blog-placeholder">
+       <div class="card-ad-content">
+         <h1 class="card-ad-title">${item.name.en}</h1>
+         <div class="card-ad-location">
+           <img class="card-ad-location-icon" src="${baseURL}/location-icon.svg" alt="location_icon">
+           <p class="card-ad-location-text">${
+             item.location.district.name.en + ', ' + item.location.state.name.en
+           }</p>
+         </div>
+            <div class="card-ad-review">
                 <div class="star-rating">
-                  ${generateStarRating(ratingSecond)}
+                    ${generateStarRating(item.ranking.user_rating ?? 0)}
                 </div>
-               <p class="card-ad-review-text"><span id="rating value">${ratingSecond}</span> / 5 <span class="card-ad-review-count">(243 Reviews)</span></p>
-             </div>
-             <div class="card-ad-tag-list">
-               <div class="card-ad-tag-bubble">Bathrooms</div>
-               <div class="card-ad-tag-bubble">Bedrooms</div>
-               <div class="card-ad-tag-bubble">Parking</div>
-             </div>
-             <div class="card-ad-available">
-               <div class="card-ad-available-text">Available date</div>
-               <p class="card-ad-available-date">10-12-2024</p>
-             </div>
-             <div class="card-ad-button"><span class="card-ad-button-text">HK$ 50</span></div>
-           </div>
-         </a>
-          `);
-        case 'three':
-          // const {results} = await callHotelData({
-          //   searches: [
-          //     {
-          //       collection: 'space_HK_1_PROD',
-          //       page: 1,
-          //       include_fields:
-          //         'slug,_id,name,location.area,location.district,location.country,location.city,location.state,_geoloc,_tags,tags,images,ranking,config,id,top_comments,top_comments_average_rating,status.min_prices,frame_style,status',
-          //       per_page: 10,
-          //       query_by:
-          //         'name,location.address,location.area.name,location.district.name,location.meta_district.name,location.city.name,location.state.name,amenities.name,tags.content',
-          //       sort_by:
-          //         '_text_match:desc,_eval([(ranking.is_promotion:true):10,(ranking.is_featured:true):9]):desc,ranking.normalized_score:desc',
-          //       override_tags: 'deals_today-s-deal',
-          //       filter_by:
-          //         '_tags:=live && config.show.is_web_show:=true && (config.show.status:!=UNLISTED) && ((tags.slug:=today-s-deal))',
-          //       q: search || '*',
-          //     },
-          //   ],
-          // });
-
-          const data = await callHotelData({
-            space_type_tab: 'rest',
-            sort_by: 's_default',
-            per_page: 6,
-            page: 1,
-            include_buttons: false,
-            query: {
-              id,
-              type: type === 'NAME' ? 'SPACE' : type,
-            },
-          });
-          console.log(data, 'data');
-          const {spaces} = data;
-          const adElementHref = ``;
-          const cards = spaces.map(
-            (item) => `<a href="https://app.flowtheroom.com/hk/en/${
-              item.slug
-            }/hotel" target="_blank" class="card-ad">
-           <img class="card-ad-img" src="${
-             item.images[0]
-           }" alt="blog-placeholder">
-           <div class="card-ad-content">
-             <h1 class="card-ad-title">${item.name.en}</h1>
-             <div class="card-ad-location">
-               <img class="card-ad-location-icon" src="${baseURL}/location-icon.svg" alt="location_icon">
-               <p class="card-ad-location-text">${
-                 item.location.district.name.en +
-                 ', ' +
-                 item.location.state.name.en
-               }</p>
-             </div>
-                <div class="card-ad-review">
-                    <div class="star-rating">
-                        ${generateStarRating(item.ranking.user_rating ?? 0)}
-                    </div>
-                    <p class="card-ad-review-text"><span id="rating value">${
-                      item.ranking.user_rating ?? 0
-                    }</span> / 5 <span class="card-ad-review-count">(${
-              item.ranking.total_comments
-            } Reviews)</span></p>
-                  </div>
-             <div class="card-ad-tag-list">
-              ${item.tags
-                .map(
-                  (tag) =>
-                    `<div class="card-ad-tag-bubble">${tag.content.en}</div>`
-                )
-                .join('')}
-             </div>
-             <div class="card-ad-available">
-               <div class="card-ad-available-text">Available date</div>
-               <p class="card-ad-available-date">${convertTimestampToDate(
-                 item.next_available
-               )}</p>
-             </div>
-             <div class="card-ad-button"><span class="card-ad-button-text">HK$ 50</span></div>
-           </div>
-         </a>`
-          );
-          return (adElement.innerHTML = `
-              <div class="card-ad-container">
-                  ${cards.join('')}
+                <p class="card-ad-review-text"><span id="rating value">${
+                  item.ranking.user_rating ?? 0
+                }</span> / 5 <span class="card-ad-review-count">(${
+          item.ranking.total_comments
+        } Reviews)</span></p>
               </div>
-              `);
-      }
+         <div class="card-ad-tag-list">
+          ${item.tags
+            .map(
+              (tag) => `<div class="card-ad-tag-bubble">${tag.content.en}</div>`
+            )
+            .join('')}
+         </div>
+         <div class="card-ad-available">
+           <div class="card-ad-available-text">Available date</div>
+           <p class="card-ad-available-date">${convertTimestampToDate(
+             item.next_available
+           )}</p>
+         </div>
+         <div class="card-ad-button"><span class="card-ad-button-text">HK$ 50</span></div>
+       </div>
+     </a>`
+      );
+      return (adElement.innerHTML = `
+          <div class="card-ad-container">
+              ${cards.join('')}
+          </div>
+          `);
     }
     handleRenderCard();
   });
